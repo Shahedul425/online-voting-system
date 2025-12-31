788aea085ea6883ce269745f4fefbf11a97b5913
Shahed <islamshahedul537@gmail.com>
2025-12-31 12:25:33 +0000

    Implement Merkel tree and Organization Service

--- OnlineVotingSystem/Backend/demo/src/main/java/com/example/demo/Repositories/VoteModelRepository.java
+++ OnlineVotingSystem/Backend/demo/src/main/java/com/example/demo/Repositories/VoteModelRepository.java
@@ -2,10 +2,16 @@ package com.example.demo.Repositories;
 
 import com.example.demo.Models.VoteModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface VoteModelRepository extends JpaRepository<VoteModel, UUID> {
+    public Optional<VoteModel> findById(UUID id);
+    @Query("select vt.receiptHashToken from VoteModel vt where vt.electionId = :id")
+    public List<String> findReceiptTokensByElectionId(UUID id);
 }
