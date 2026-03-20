cf7be0060f969fb4c240549cf71ca75ef500261f
Shahed <islamshahedul537@gmail.com>
2026-03-20 22:57:44 +0000

    Initial Deployment Setup

--- OnlineVotingSystem/Frontend/my-react-app/src/Service/Api/loginApi.js
+++ OnlineVotingSystem/Frontend/my-react-app/src/Service/Api/loginApi.js
@@ -1,11 +1,13 @@
+const BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:8080";
 
-export async function  loginRequest (username,password){
-    const response = await fetch("http://localhost:8080/public/auth/login",{
-        method:"POST",
-        headers:{"Content-Type":"application/json"},
-        body:JSON.stringify({username,password}),
-        credentials:"include"
-    })
-    if (!response.ok) throw new Error("Login failed!!")
+export async function loginRequest(username, password) {
+    const response = await fetch(`${BASE_URL}/public/auth/login`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ username, password }),
+        credentials: "include"
+    });
+
+    if (!response.ok) throw new Error("Login failed!!");
     return await response.json();
 }
--- OnlineVotingSystem/Frontend/my-react-app/src/Service/Api/registerApi.js
+++ OnlineVotingSystem/Frontend/my-react-app/src/Service/Api/registerApi.js
@@ -1,5 +1,7 @@
+const BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:8080";
+
 export async function registerRequest(email, username, firstName, lastName, password) {
-    const response = await fetch("http://localhost:8080/public/auth/register", {
+    const response = await fetch(`${BASE_URL}/public/auth/register`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ username, email, firstName, lastName, password }),
@@ -7,7 +9,6 @@ export async function registerRequest(email, username, firstName, lastName, pass
     });
 
     if (response.status === 409) {
-        // email exists
         const data = await response.json();
         throw new Error(data.error);
     }
